777c9672ff86576614c53a8ff2578dc01d1438e0
Devin Taylor <dtaylor@isaziconsulting.co.za>
2019-05-02 01:34:11 +0100

    added init imports at root level

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+import multiaug.augmenters.image3d as image3d_augmenters
+import multiaug.augmenters.tabular as tabular_augmenters
+import multiaug.augmenters as augmenters
+
+__version__ = '0.1.0'
